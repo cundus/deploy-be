017e3dbd0487f9126d710edc4445a89108506f35
Sundus <pcinta0@gmail.com>
2024-09-02 10:37:47 +0700

    adding cors

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World from deployed versioon!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
