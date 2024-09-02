897a48eb9173712cb4b1b67666a379680a694493
Sundus <pcinta0@gmail.com>
2024-09-02 14:12:03 +0700

    cloudinary

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import multer from "multer";
+import path from "path";
+import { uploadCloudinary } from "./cloudinary";
 
 const client = new PrismaClient();
+import {
+   v2 as cloudinary,
+   UploadApiResponse,
+   UploadApiErrorResponse,
+} from "cloudinary";
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+interface CloudinaryFile extends Express.Multer.File {
+   buffer: Buffer;
+}
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
 
 const app = express();
 const port = 3000;
@@ -18,4 +40,27 @@ app.get("/", async (req, res) => {
 
    res.send(users[0].name);
 });
+
+app.post(
+   "/upload",
+   upload.single("file"),
+   uploadCloudinary,
+   async (req, res) => {
+      try {
+         const file = res.locals.image;
+         const newUser = await client.user.create({
+            data: {
+               name: file,
+               age: 0,
+            },
+         });
+
+         res.send(newUser.name);
+      } catch (error) {
+         console.error(error);
+         res.send("Error uploading image");
+      }
+   }
+);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
